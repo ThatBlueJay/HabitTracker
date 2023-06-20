5c931a8a8f005d42354ab3c6b8e1a49d1733005a
thezw27 <134734870+thezw27@users.noreply.github.com>
2023-06-19 20:27:34 -0400

    Create HabitTrackerDBSetup.sql

---
+++ Database/HabitTrackerDBSetup.sql
@@ -0,0 +1,36 @@
+include interval;
+
+CREATE TABLE Class (
+    class_id SERIAL PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    start_time timestamp,
+    end_time timestamp,
+    end_date timestamp,
+    recurring INTERVAL
+);
+
+CREATE TABLE Habits (
+    habit_id SERIAL PRIMARY KEY,
+    title VARCHAR(280) UNIQUE NOT NULL,
+    description VARCHAR(512),
+    start_time timestamp,
+    end_time timestamp,
+    category VARCHAR(100),
+    recurring INTERVAL,
+    end_date timestamp,
+    user_id INT NOT NULL,
+    class_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (class_id) REFERENCES Class(class_id)
+);
+
+CREATE TABLE Records (
+    record_id SERIAL PRIMARY KEY,
+    date_complete timestamp NOT NULL,
+    due_date timestamp NOT NULL,
+    complete bool NOT NULL,
+    complete_on_time bool NOT NULL,
+    hours_spent INT NOT NULL,
+    habit_id INT NOT NULL,
+    FOREIGN KEY (habit_id) REFERENCES Habits(habit_id)
+);
