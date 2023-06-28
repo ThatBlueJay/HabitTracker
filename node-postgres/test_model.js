460201a2235d272ce3842bd40156e2229d98951a
ThatBlueJay <61397831+ThatBlueJay@users.noreply.github.com>
2023-06-28 16:17:43 -0400

    Added simple functions for creating, deleting, and listing habits

--- node-postgres/test_model.js
+++ node-postgres/test_model.js
@@ -7,41 +7,44 @@ const pool = new Pool({
   port: 5432,
 });
 
-const getMerchants = () => {
-    return new Promise(function(resolve, reject) {
-      pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(results.rows);
-      })
-    }) 
-  }
-  const createMerchant = (body) => {
-    return new Promise(function(resolve, reject) {
-      const { name, email } = body
-      pool.query('INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(`A new merchant has been added added: ${results.rows[0]}`)
-      })
+const createHabit = (body) => {
+  return new Promise(function(resolve, reject) {
+    const {habit_id, title, description, start_time, end_time, category, recurring, end_date, user_id, class_id} = body
+    pool.query('INSERT INTO habits (habit_id, title, description, start_time, end_time, category, recurring, end_date, user_id, class_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *', [habit_id, title, description, start_time, end_time, category, recurring, end_date, user_id, class_id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new habit has been added added: ${results.rows[0]}`)
     })
-  }
-  const deleteMerchant = () => {
-    return new Promise(function(resolve, reject) {
-      const id = parseInt(request.params.id)
-      pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(`Merchant deleted with ID: ${id}`)
-      })
+  })
+}
+
+const deleteHabit = () => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM habits WHERE habit_id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Habit deleted with ID: ${id}`)
     })
-  }
+  })
+}
+
+const getHabit = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM habits ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
   
   module.exports = {
-    getMerchants,
-    createMerchant,
-    deleteMerchant,
+    createHabit,
+    deleteHabit,
+    getHabit,
   }
