8673ae108b9ed2355fea2aeecd6f80df7d47bf14
Becky Chen <chenb9@rpi.edu>
2023-07-10 20:08:25 -0400

    calendar and profile page

--- habittracker/src/App.js
+++ habittracker/src/App.js
@@ -9,22 +9,57 @@ import Analytics from './pages/Analytics';
 import Profile from './pages/Profile';
 import { ChakraProvider } from '@chakra-ui/react'
 
+/*
+Check to see if the user is logged in
+*/
+var login = true;
+var id = -1;
+
 function App() {
-  return (
-    <ChakraProvider>
-      <Router>
-        <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Habits" element={<Habits />} />
-            <Route path="/Calendar" element={<Calendar />} />
-            <Route path="/Analytics" element={<Analytics />} />
-            <Route path="/Profile" element={<Profile />} />
-          </Routes>
-        <Footer />
-      </Router>
-    </ChakraProvider>
-  );
+
+  var data = null;
+
+  if(login) {
+    data =  getUserData(id);
+  }
+
+  var username = login ? data.username : "none";
+  var email = login ? data.email : "none";
+  var phone = login ? data.phone : "none";
+
+    return (
+      <ChakraProvider>
+        <Router>
+          <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Habits" element={login ? <Habits /> : <Home/>} />
+              <Route path="/Calendar" element={login ? <Calendar data={getHabitData(id)}/> : <Home/>} />
+              <Route path="/Analytics" element={login ? <Analytics /> : <Home/>} />
+              <Route path="/Profile" element={login ? <Profile username={username} email={email} phone={phone}/> : <Home/>} />
+            </Routes>
+          <Footer />
+        </Router>
+      </ChakraProvider>
+    );
+  }
+
+
+
+function getUserData(id) {
+  if(login) {
+    fetch("/users/" + id)
+      .then(data => data.json())
+      .then(success => {console.log(success)})
+  }
 }
+  
+async function getHabitData(id) {
+  await fetch("/habits/" + id)
+    .then(data => data.json())
+    .then(success => {console.log(success);})
+  }
+
+
 
 export default App;
