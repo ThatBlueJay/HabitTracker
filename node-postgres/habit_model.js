878f5f95f8f7b23da40840e3274ec1d56c386530
ThatBlueJay <61397831+ThatBlueJay@users.noreply.github.com>
2023-07-19 21:09:53 -0400

    Confirmed that checking future habits now does work and it also marks them properly

--- node-postgres/habit_model.js
+++ node-postgres/habit_model.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 
 const createHabit = (body) => {
   return new Promise(function(resolve, reject) {
-    const {title, description, start_time, end_time, category, recurring, start_date, end_date, user_id, class_id} = body
+    const {title, description, start_time, end_time, category, recurring, start_date, end_date, user_id} = body
     pool.query('INSERT INTO habits VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, null) RETURNING habit_id', [title, description, start_time, end_time, category, recurring, start_date, end_date, user_id], (error, results) => {
       if (error) {
         reject(error)
