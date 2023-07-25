21f36f88bc0b2dc191dc002e77a10334b93093c2
Smolae <smolae@rpi.edu>
2023-07-25 16:03:39 -0400

    Update Habits.jsx

--- habittracker/src/pages/Habits.jsx
+++ habittracker/src/pages/Habits.jsx
@@ -1,20 +1,68 @@
-//import React from "react";
-import styled from "styled-components";
-//  const {title, description, start_time, end_time, category, recurring, start_date, end_date, user_id} = body
-import React, { useContext } from "react";
-import { LoginContext } from "../App.js"; 
+import React, { useState, useContext } from "react";
+import { LoginContext, IdContext } from "../App.js"; 
 import { Navigate } from "react-router-dom";
+import styled from "styled-components";
 
 function Habits() {
   const { login } = useContext(LoginContext);
+  const { id } = useContext(IdContext);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [category, setCategory] = useState('');
+  const [recurring, setRecurring] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
 
   // if the user is not logged in, redirect them to the home page
+  /*
   if (!login) {
     return <Navigate to="/" />
+  }*/
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const habit = {
+      title,
+      description,
+      start_time: startTime,
+      end_time: endTime,
+      category,
+      recurring: {
+        days: recurring
+      },
+      start_date: startDate,
+      end_date: endDate,
+      user_id: id
+    };
+
+    try {
+      const response = await fetch('/habits', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(habit)
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || 'Could not save the habit.');
+      }
+
+      // You can add some code here to handle the response
+    } catch (err) {
+      console.error(err);
+      // Handle the error here
+    }
   }
 
   return(
-    <HabitContainer>
+    <HabitContainer onSubmit={handleSubmit}>
       <Column>
       <Header>Add a habit here!</Header>
       <Subheader>Please enter the days and times you would like to complete this habit</Subheader>
@@ -22,16 +70,62 @@ function Habits() {
       </Column>
       <Column>
       <h1>Habit Name</h1>
-      <input type="text" placeholder="Enter Habit Here" />
+      <input 
+        type="text" 
+        placeholder="Enter Habit Here"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
       <h1>Interval</h1>
-      <p><input type="text" placeholder="1"/> every   <input type="text" placeholder="Wednesday"/></p>
+      <p>
+        <input 
+          type="text" 
+          placeholder="1"
+          value={recurring}
+          onChange={e => setRecurring(e.target.value)}
+        /> 
+        every   
+        <input 
+          type="text" 
+          placeholder="Wednesday"
+          value={category}
+          onChange={e => setCategory(e.target.value)}
+        />
+      </p>
       <p>Start Date</p>
-      
+      <input 
+        type="date" 
+        value={startDate}
+        onChange={e => setStartDate(e.target.value)}
+      />
+      <p>End Date</p>
+      <input 
+        type="date" 
+        value={endDate}
+        onChange={e => setEndDate(e.target.value)}
+      />
+      <p>Start Time</p>
+      <input 
+        type="time" 
+        value={startTime}
+        onChange={e => setStartTime(e.target.value)}
+      />
+      <p>End Time</p>
+      <input 
+        type="time" 
+        value={endTime}
+        onChange={e => setEndTime(e.target.value)}
+      />
+      <p>Description</p>
+      <textarea 
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      /><br />
+      <button type="submit">Save Habit</button>
       </Column>
     </HabitContainer>
   );
 }
-
 const HabitContainer = styled.div`
   position: relative;
   overflow: hidden;
