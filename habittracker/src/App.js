e6d5e383539a990c64b72f880e3997714471a282
Smolae <smolae@rpi.edu>
2023-07-26 12:21:23 -0400

    Revert "Merge branch 'smolae'"
    This reverts commit a2112812a730c959072667f96e3c3365200f3bb5, reversing
    changes made to e3dcdd2957bea4a79cca11a59d178cf0e938bd7e.

--- habittracker/src/App.js
+++ habittracker/src/App.js
@@ -7,70 +7,124 @@ import Habits from './pages/Habits';
 import Calendar from './pages/Calendar';
 import Analytics from './pages/Analytics';
 import Profile from './pages/Profile';
-import Signup from './pages/Signup';
 import { ChakraProvider } from '@chakra-ui/react'
-import { useContext, createContext, useState } from "react";
 
-export const LoginContext = createContext();
-export const IdContext = createContext();
+/*
+Check to see if the user is logged in
+*/
+var login = true;
+var id = -1;
 
-function AppProvider({ children }) {
-  const [login, setLogin] = useState(false);
-  const [id, setId] = useState(null);
-
-  const handleLogin = (userId) => {
-    setLogin(true);
-    setId(userId);
-  };
-
-  return (
-    <LoginContext.Provider value={{ login, handleLogin }}>
-      <IdContext.Provider value={{ id, setId }}>
-        {children}
-      </IdContext.Provider>
-    </LoginContext.Provider>
-  );
-}
-var data;
 function App() {
-  //const { login } = useContext(LoginContext);
-  //const {id} = useContext(IdContext);
+
+  //This is for testing purposes only
+  const allHabits = [
+    {
+        "habit_id": 1,
+        "title": "amogus",
+        "description": "play",
+        "start_time": "16:00:00",
+        "end_time": "18:00:00",
+        "category": "alls",
+        "recurring": {
+            "days": 7
+        },
+        "start_date": "2023-06-01T04:00:00.000Z",
+        "end_date": "2023-07-15T04:00:00.000Z",
+        "user_id": 1,
+        "class_id": null
+    },
+    {
+        "habit_id": 2,
+        "title": "fortnite",
+        "description": "john wick",
+        "start_time": "16:00:00",
+        "end_time": "18:00:00",
+        "category": "alls",
+        "recurring": {
+            "days": 7
+        },
+        "start_date": "2023-06-01T04:00:00.000Z",
+        "end_date": "2023-07-15T04:00:00.000Z",
+        "user_id": 1,
+        "class_id": null
+    },
+    {
+        "habit_id": 6,
+        "title": "fnaf",
+        "description": "frazbear",
+        "start_time": "16:00:00",
+        "end_time": "18:00:00",
+        "category": "alls",
+        "recurring": {
+            "seconds": 7
+        },
+        "start_date": "2023-07-01T04:00:00.000Z",
+        "end_date": "2023-08-12T04:00:00.000Z",
+        "user_id": 1,
+        "class_id": null
+    },
+    {
+        "habit_id": 7,
+        "title": "revengeance",
+        "description": "play the game its fun",
+        "start_time": "10:00:00",
+        "end_time": "12:00:00",
+        "category": "alls",
+        "recurring": {
+            "days": 7
+        },
+        "start_date": "2023-07-19T04:00:00.000Z",
+        "end_date": "2023-08-19T04:00:00.000Z",
+        "user_id": 1,
+        "class_id": null
+    }
+  ];
+
+  var data = null;
+
+  if(login) {
+    data =  getUserData(id);
+  }
+
+  //var username = login ? data.username : "none";
+  //var email = login ? data.email : "none";
+  //var phone = login ? data.phone : "none";
+
     return (
-      <AppProvider>
       <ChakraProvider>
         <Router>
           <Header />
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/Habits" element={<Habits />} />
-              <Route path="/Calendar" element={<Calendar data={getHabitData(id)}/>} />
-              <Route path="/Analytics" element={<Analytics />} />
-              <Route path="/Profile" element={<Profile username={"username"} email={"email"} phone={"911"}/>} />
-              <Route path="/Signup" element={<Signup />} />
+              <Route path="/Habits" element={login ? <Habits /> : <Home/>} />
+              <Route path="/Calendar" element={login ? <Calendar data={allHabits}/> : <Home/>} />
+              <Route path="/Analytics" element={login ? <Analytics /> : <Home/>} />
+              <Route path="/Profile" element={login ? <Profile username={"username"} email={"email"} phone={"911"}/> : <Home/>} />
             </Routes>
           <Footer />
         </Router>
       </ChakraProvider>
-      </AppProvider>
     );
   }
 
+/* */
 
-/*
-function getUserData(id) {
+async function getUserData(id) {
   if(login) {
-    fetch("/users/" + id)
+    await fetch("/users/" + id)
       .then(data => data.json())
       .then(success => {console.log(success)})
   }
 }
-  
+
+//This returns all of the habits for the specific user
 async function getHabitData(id) {
   await fetch("/habits/" + id)
     .then(data => data.json())
-    .then(success => {console.log(success);})
+    .then(success => {console.log(success)})
   }
-*/
+
 
 
 export default App;
--- habittracker/src/pages/Calendar.jsx
+++ habittracker/src/pages/Calendar.jsx
@@ -1,8 +1,6 @@
-import React, { useState, useRef, useContext } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { DayPilotCalendar, DayPilotNavigator } from "@daypilot/daypilot-lite-react";
 import styled from "styled-components";
-import { LoginContext } from "../App.js"; 
-import { Navigate } from "react-router-dom";
 
 const styles = {
   wrap: {
@@ -16,12 +14,69 @@ const styles = {
   }
 };
 
-const Calendar = () => {
-  const { login } = useContext(LoginContext);
+function getMonthName(monthNumber) {
+  const date = new Date();
+  date.setMonth(monthNumber - 1);
+  return date.toLocaleString('en-US', { month: 'long' });
+}
 
-  // if the user is not logged in, redirect them to the home page
-  
+function formatData(title, start, end, test) {
+  const data = [
+    {
+        "record_id": 385484,
+        "datet_complete": null,
+        "due_date": "2023-07-19T16:00:00.000Z",
+        "complete": false,
+        "complete_on_time": null,
+        "hours_spent": 0,
+        "habit_id": 7
+    },
+    {
+        "record_id": 385485,
+        "datet_complete": "2023-07-20T01:09:07.702Z",
+        "due_date": "2023-07-26T16:00:00.000Z",
+        "complete": true,
+        "complete_on_time": true,
+        "hours_spent": 159,
+        "habit_id": 7
+    }
+  ];
+  let toAdd = [];
+  for(let i = 0; i < data.length; i++) {
+    toAdd = toAdd.concat({
+      text: title,
+      start: data[i].due_date.substring(0,11) + start,
+      end: data[i].due_date.substring(0,11) + end,
+      backColor: "#f1c232"
+    });
+  }
+  return toAdd;
+}
 
+async function getHabits(props) {
+  const today = new Date();
+
+  const start = getMonthName(today.getMonth()+1) + " " + 1 + ", " + today.getFullYear();
+  const end = getMonthName(today.getMonth()+3) + " " + 1 + ", " + today.getFullYear();
+
+  var allHabitsToPutOnCalendar = [];
+
+  for(let i = 0; i < props.data.length; i++) {
+    const habitID = props.data[i].habit_id;
+    let data = [];
+    allHabitsToPutOnCalendar = allHabitsToPutOnCalendar.concat(formatData(props.data[i].title, props.data[i].start_time, props.data[i].end_time, data));
+
+    // await fetch("/habits/?id=" + habitID + "&begin=" + start + "&end=" + end) 
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     allHabitsToPutOnCalendar = allHabitsToPutOnCalendar.concat(formatData(props.data[i].title, props.data[i].start_time, props.data[i].end_time, data));
+    //     console.log(data.json());
+    //   })
+  }
+  return allHabitsToPutOnCalendar;
+}
+
+const Calendar = (props) => {
     const [config, setConfig] = useState({
       viewType: "Week",
       durationBarVisible: false
@@ -34,9 +89,17 @@ const Calendar = () => {
         startDate: args.day
       });
     }
-    if (!login) {
-      return <Navigate to="/" />
-    }
+
+    useEffect(() => {
+      //const habits = Object.keys(getHabits(props));
+      async function updateCalendar() {
+        const events = await getHabits(props);
+        const startDate = new Date();
+        console.log(startDate);
+        calendarRef.current.control.update({startDate, events});
+      }
+      updateCalendar();
+    }, []);
 
     return (
       <CalendarContainer>
