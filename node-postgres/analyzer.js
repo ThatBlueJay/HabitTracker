bda10e83c269dcb9357c854500db401e7f082a77
ThatBlueJay <61397831+ThatBlueJay@users.noreply.github.com>
2023-08-02 21:37:31 -0400

    Revamped analytics data to consistency function

--- node-postgres/analyzer.js
+++ node-postgres/analyzer.js
@@ -11,6 +11,30 @@ class Record {
     getid() {
         return this.id;
     }
+
+    getX(start, end) {
+        const totalTime = end - start;
+        const duepos = this.duedate - start;
+        const pos = duepos / totalTime;
+        return pos;
+    }
+
+    getY() {
+        const currentDate = new Date();
+        const dueDate = new Date(this.duedate)
+        if(this.completetimed) {
+            return 1;
+        }
+        else if(currentDate > dueDate && !this.complete) {
+            return 0;
+        }
+        else if (currentDate > dueDate && this.complete && !this.completetimed) {
+            return Math.exp(-0.1 * this.hours);
+        }
+        else {
+            return 0.5;
+        }
+    }
 }
 
 class Analyzer {
@@ -20,6 +44,33 @@ class Analyzer {
 
     addRecord(record) {
         this.records.push(record)
+    }
+
+    getEarliest() {
+        if (this.records.length === 0) {
+            return null; // Return null if the recordsList is empty
+        }
+      
+        let earliestRecord = this.records[0].duedate;
+        for (let i = 1; i < this.records.length; i++) {
+            if (this.records[i].duedate < earliestRecord) {
+                earliestRecord = this.records[i].duedate;
+            }
+        }
+        return earliestRecord;
+    }
+
+    getLatest() {
+        if (this.records.length === 0) {
+          return null; // Return null if the recordsList is empty
+        }
+        let latestRecord = this.records[0].duedate;
+        for (let i = 1; i < this.records.length; i++) {
+          if (this.records[i].duedate > latestRecord) {
+            latestRecord = this.records[i].duedate;
+          }
+        }
+        return latestRecord;
     }
 
     getAverage() {
@@ -36,6 +87,45 @@ class Analyzer {
         });
         return pairs;
     }
+
+    getRawData() {
+        const start = this.getEarliest()
+        const end = this.getLatest()
+        //console.log(records[0].getX(start, end))
+        const pairs = this.records.map((record) => {
+            return {x: record.getX(start, end), y: record.getY()};
+        });
+        return pairs;
+    }
+
+    getAveragedData() {
+        const resultPairs = [];
+        for(let position  = 0; position  <= 1; position  += 0.1) {
+            const lowerBound  = position - 0.05;
+            const upperBound  = position + 0.05;
+
+            const filteredPairs = this.getRawData().filter((pair) => {
+                const pairPosition = pair.x;
+                return pairPosition >= lowerBound && pairPosition <= upperBound;
+            });
+
+            const totalHours =filteredPairs.reduce((sum, pair) => sum + pair.y, 0);
+            const averageHours = filteredPairs.length > 0 ? totalHours / filteredPairs.length : 0;
+
+            resultPairs.push({
+                x: position.toFixed(2),
+                y: averageHours.toFixed(2),
+            });
+        }
+        return resultPairs;
+    }
+
+    getData() {
+        const pairs = this.getAveragedData();
+        return pairs;
+    }
+
+
 }
 
 module.exports = { Analyzer, Record};
